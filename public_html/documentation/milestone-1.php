ce8718801cd72e8012504c1244ba3f09006ebba6
merri zibert <mzibert@cnm.edu>
2016-05-02 13:25:51 -0600

    built new repository and project to rid old project of ,idea file

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,66 @@
+<!DOCTYPE>
+<html lang ="en">
+	<head>
+		<meta charset="utf-<8"/>
+		<title> Merri J. Zibert </title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<h2>Purpose:</h2>
+		<p> This project is a professional website where future employers and clients can gain a better sense of who I am both personally and professionally.  A place where they can see my professional portfolio and get a better sense of who I am both as a developer and as an individual.
+		</p>
+		<h2>Audience:</h2>
+		<p>The audience will include professional peers, possible employers and probable clients.
+		</p>
+		<h2>Goal:</h2>
+		<p>To acquire full time work both as a freelance web developer for smaller business clientele, and as a junior developer with a small privately held company who offers me the ability to keep on learning new coding talents while working my way up into a senior developer position.
+		</p>
+		<h1>Persona:</h1>
+		<h2>Name:</h2>
+		<p>Doug Sheppler</p>
+		<h2>Age:</h2>
+		<p>52</p>
+		<h2>Occupation:</h2>
+		<p>Hiring Manager for GreaterGood.org</p>
+		<h2>Technology:</h2>
+		<p> Doug is very comfortable with technology, and describes himself as an advanced
+			user.  Doug currently uses a Samsung Galaxy S6 Edge for both personal and business
+			cell phone use.  For traveling, Doug carries the new 12.9-inch iPad Pro with the smart
+			keyboard.  When he is at work Doug uses a Lenovo Think Pad with docking station.  Doug
+			uses Google Chrome as his default browser, and hates all things Microsoft.
+		</p>
+			<h2>Background:</h2>
+		<p>  Doug has been a hiring manager for 1o years now.  He has worked for some of the
+			biggest names in the technology world, like Microsoft and Oracle. He primarily
+			hired for technical positions, but has done a few non-technical hiring events.  He
+			just recently downshifted from the fast paced tech companies to a large 501C not
+			for profit company, and has been tasked with hiring a team of five junior software
+			developers. Doug will be sharing the final hiring decisions with the Senior developers
+			and the blessing of the Chief Technology Officer.
+		</p>
+		<h2>Goals:</h2>
+		<p>  Doug does not have a lot of free time to go through paper resumes, so he hopes to cut
+			down on the amount of time until hire by combing the internet for personal/professional
+			websites of junior developers that are both eye catching and full of personality.
+		</p>
+		<h1>Use Case:</h1>
+		<h2>Goal:</h2>
+		<p>  Doug wants to find junior developer personal websites that highlight who the
+		developer is in a fun, concise way.  Doug wants to see the latest coding technologies
+		represented on the site, but wants the UI to be simple while remaining visually appealing
+		</p>
+		<ol>
+			<li>0)Doug searches using his favorite browser and clicks on mjzibert.com</li>
+			<li>1)mjzibert.com opens to mostly black and white photo with some call to
+				action buttons displayed in the center</li>
+			<li>2)Doug likes the clean overall feel of the page and clicks on the
+				"get to know me" call to action button </li>
+			<li>3)the "get to know me" page is displayed where it displays two sections, "me personally"
+			and "me professionally"</li>
+			<li>4)Doug learns more about the junior developer mjzibert and her passions both
+				personally and professionally.  Doug then navigates to the develper tools</li>
+			<li>5)An up to date, well written page of code is displayed using some of the latest technologies.</li>
+			<li>Doug contacts mjzibert to proceed to the next step in the hiring process.</li>
+		</ol>
+	</body>
+</html>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 2 (alpha)</title>
+	</head>
+	<body>
+		<h1> PWP Milestone 2(alpha)</h1>
+	</body>
+	<p>
+	<h2>Proposed Site Map</h2>
+	<p>This site will consist of 3 top level pages that will give an overall picture of who I am as a person in both my professional and personal environments.</p>
+	<ol>
+	<li>Home</li>
+	<li>Work</li>
+	<li>Play</li>
+	</ol>
+	<h2>Content Strategy</h2>
+	<h3>Home</h3>
+	<p>This page will have my full name clearly displayed. there will be a brief welcome paragraph and a personal statement that will summarize who I am.  It will clearly contain two call to action buttons that will overlap to create a zen diagram look and feel. One for my work story, and one for my play story. This page will also contain all of my professional social media page links.
+	</p>
+	<h3>Work</h3>
+	<p>This page will clearly state my desire to continue my full stack development skills working freelance projects and obtaining a position for a full time junior developer. It will contain a gallery of my completed projects with each project clearly stating my role and will have a detailed list of the technologies that were used.  It will have a place for my current projects, with information about the project and the technologies being used.  It will also conatin a section to highlight my involvement with different groups with in the locl tech community.  Additionally, the bottom of this page will also contain the links and all of my professional social media pages.
+	</p>
+	<h3>Play</h3>
+	<p>This will contain a brief summary about me as a person in my off time and highlight my strengths and how they are incorporated to life outside of the office. Fitting into 3 categories; continuing to code, family, community involvement
+	</p>
+	<h2> Interaction Flow </h2>
+	<p>  Doug wants to find junior developer personal websites that highlight who the
+		developer is in a fun, concise way.  Doug wants to see the latest coding technologies
+		represented on the site, but wants the UI to be simple while remaining visually appealing
+	</p>
+	<ol>Doug has been tasked with hiring a team of five junior software developers.  Doug has decided that browsing the internet for personal pages of developers will be the most effective way to gather a pool of applicants.
+		<li>0)Doug searches using his favorite browser and clicks on mjzibert.com</li>
+		<li>1)mjzibert.com opens and Doug immeadiately sees the name prominently displayed, with a welcome pitch and a personal statement.  Doug scrolls down and sees the ven diagram call to action buttons displayed in the center</li>
+		<li>2)Doug likes the clean overall look of the page and is drawn in by the playful feel.  He clicks on the work call to action button and is taken to the work content page</li>
+		<li>3)the "work" page is displayed and the four categories are well organized and do a nice job showcasing the different projects I have worked on both past and present, as well as the community that I have involved myself in.</li>
+		<li>4)Doug learns more about the junior developer mjzibert and her passions both
+			personally and professionally.  Doug then navigates to the develper tools</li>
+		<li>5)An up to date, well written page of code is displayed using some of the latest technologies.</li>
+		<li>Doug uses the linked in link displayed at the bottom of the page to get a better feel of my past work history and contacts me to see if I would be interested in interviewing for the position.</li>
