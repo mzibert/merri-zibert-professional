64a3eac066dc2fd096818594744c1a380b30cb5e
merri zibert <mzibert@cnm.edu>
2016-05-23 09:40:04 -0600

    built new repository and project to rid old project of ,idea file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- fontawesome -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</head>
