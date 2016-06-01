ab4221abbcb1145bdf3a15154aa1b136e6b770bc
merri zibert <mzibert@cnm.edu>
2016-06-01 13:46:23 -0600

    added work and play pages

---
+++ public_html/work/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
+		<link rel="stylesheet" href="style.css" />
+		<script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script data-require="bootstrap.js@*" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="script.js"></script>
+	</head>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<!-- header -->
+			<header class="p-y-4">
+				<div class="container">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">My Site</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-nav">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link1</a></li>
+								<li><a href="#">Link2</a></li>
+								<li><a href="#">Link3</a></li>
+								<li><a href="#">Link4</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+				</div>
+			</header>
+
+			<!-- welcome section -->
+			<section id="welcome" class="p-y-4">
+				<div class="container">
+					<div class="jumbotron">
+						<h1>Welcome!</h1>
+						<p>Byline Text</p>
+					</div>
+				</div>
+			</section>
+
+			<!-- feature section -->
+			<section id="feature" class="p-y-4">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="http://placekitten.com/g/200/200" alt="kitty">
+						</div>
+						<div class="col-md-9">
+							<p>Tuxedo cats always looking dapper hola te quiero yet fall over dead (not really but gets sypathy). White cat sleeps on a black shirt chase red laser dot, leave fur on owners clothes. Thinking longingly about tuna brine meow yet eat the fat cats food lay on arms while you're using the keyboard or roll on the floor purring your whiskers off jump around on couch, meow constantly until given food, . Paw at beetle and eat it before it gets away throwup on your pillow. Stick butt in face hopped up on catnip spread kitty litter all over house stare at wall turn and meow stare at wall some more meow again continue staring find something else more interesting</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- thumbnails -->
+			<section id="thumbnails" class="p-y-4">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="thumbnail">
+								<img src="http://placekitten.com/g/100/100" alt="" class="img-responsive img-thumbnail img-circle">
+								<div class="caption">
+									<h3>Title 1</h3>
+									<p> Immediately regret falling into bathtub unwrap toilet paper yet spread kitty litter all over house, and touch water with paw then recoil in horror leave hair everywhere lick butt and make a weird face stretch</p>
+								</div>
+							</div><!--/.thumbnail-->
+						</div><!--/.col-md-3-->
+						<div class="col-md-3">
+							<div class="thumbnail">
+								<img src="http://placekitten.com/g/100/100" alt="" class="img-responsive img-thumbnail img-circle">
+								<div class="caption">
+									<h3>Title 2</h3>
+									<p> Immediately regret falling into bathtub unwrap toilet paper yet spread kitty litter all over house, and touch water with paw then recoil in horror leave hair everywhere lick butt and make a weird face stretch</p>
+								</div>
+							</div><!--/.thumbnail-->
+						</div><!--/.col-md-3-->
+						<div class="col-md-3">
+							<div class="thumbnail">
+								<img src="http://placekitten.com/g/100/100" alt="" class="img-responsive img-thumbnail img-circle">
+								<div class="caption">
+									<h3>Title 3</h3>
+									<p> Immediately regret falling into bathtub unwrap toilet paper yet spread kitty litter all over house, and touch water with paw then recoil in horror leave hair everywhere lick butt and make a weird face stretch</p>
+								</div>
+							</div><!--/.thumbnail-->
+						</div><!--/.col-md-3-->
+						<div class="col-md-3">
+							<div class="thumbnail">
+								<img src="http://placekitten.com/g/100/100" alt="" class="img-responsive img-thumbnail img-circle">
+								<div class="caption">
+									<h3>Title 4</h3>
+									<p> Immediately regret falling into bathtub unwrap toilet paper yet spread kitty litter all over house, and touch water with paw then recoil in horror leave hair everywhere lick butt and make a weird face stretch</p>
+								</div>
+							</div><!--/.thumbnail-->
+						</div><!--/.col-md-3-->
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</section>
+
+		</div>
+		<footer class="p-y-4">
+			<div class="container">
+				<div class="copyright text-center">
+					&copy; Foobar Productions
+				</div>
+			</div>
+		</footer>
+	</body>
+
+</html>
