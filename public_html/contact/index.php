a33a7cb96ed0e5029b22d68ce0c0044a902ef1b8
merri zibert <mzibert@cnm.edu>
2016-06-10 13:40:48 -0600

    doing work on contact form

--- public_html/contact/index.php
+++ public_html/contact/index.php
@@ -3,13 +3,16 @@
 	<head>
 		<meta charset="utf-8">
 
-		<link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
+		<link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css"/>
 
 		<link rel="stylesheet" href="../css/styles.css" type="text/css">
 
-		<script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script data-require="jquery@*" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
-		<script data-require="bootstrap.js@*" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script data-require="bootstrap.js@*" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 		<script src="script.js"></script>
 
@@ -17,9 +20,12 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -38,7 +44,8 @@
 					<div class="container">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#main-nav" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
@@ -59,56 +66,59 @@
 					</div><!-- /.container-fluid -->
 				</nav>
 			</header>
-			
-			<!--Begin Contact Form-->
-<form id="contact-form" action="php/mailer.php" method="post">
-	<div class="form-group">
-		<label for="name">Name <span class="text-danger">*</span></label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<i class="fa fa-user" aria-hidden="true"></i>
-			</div>
-			<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="email">Email <span class="text-danger">*</span></label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<i class="fa fa-envelope" aria-hidden="true"></i>
-			</div>
-			<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="subject">Subject</label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<i class="fa fa-pencil" aria-hidden="true"></i>
-			</div>
-			<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="message">Message <span class="text-danger">*</span></label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<i class="fa fa-comment" aria-hidden="true"></i>
-			</div>
-			<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
-		</div>
-	</div>
+			<!---Begin Contact Form-->
+			<div class="row">
+				<div class="col-md-6 col-sm-3 col-md-7 ">
+					<form id="contact-form" action="php/mailer.php" method="post">
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message"
+											 placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
+				</div>
 
-	<!-- reCAPTCHA -->
-	<div class="g-recaptcha" data-sitekey="6LedHyITAAAAAMdLdDHkYglXd58zwYrFnmDjeNz4"></div>
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6LedHyITAAAAAMdLdDHkYglXd58zwYrFnmDjeNz4"></div>
 
-	<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-	<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-</form>
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
 
-<!--empty area for form error/success output-->
-<div class="row">
-	<div class="col-xs-12">
-		<div id="output-area"></div>
-	</div>
-</div>
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
